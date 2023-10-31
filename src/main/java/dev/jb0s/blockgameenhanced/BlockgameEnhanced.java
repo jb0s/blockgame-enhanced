676a4b9913ff5fbc534748e50f42aba966f018a3
Jake <68775263+jb0s@users.noreply.github.com>
2023-10-31 19:30:25 +0100

    fixed crash

--- src/main/java/dev/jb0s/blockgameenhanced/BlockgameEnhanced.java
+++ src/main/java/dev/jb0s/blockgameenhanced/BlockgameEnhanced.java
@@ -18,7 +18,7 @@ import java.nio.file.Path;
 public class BlockgameEnhanced implements ModInitializer {
 
     public static final Logger LOGGER = LoggerFactory.getLogger("blockgameenhanced");
-    public static final boolean DEBUG = System.getenv("bge-debug").equals("true");
+    public static final boolean DEBUG = System.getenv("bge-debug") != null;
 
     @Getter
     private static boolean modmenuPresent = false;
