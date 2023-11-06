589c3a8680b139db2442834e4c88e6a82eaa4524
Jake <68775263+jb0s@users.noreply.github.com>
2023-11-06 21:50:42 +0100

    Personalized vendor themes

---
+++ src/main/java/dev/jb0s/blockgameenhanced/manager/mmocore/vendor/MMOVendor.java
@@ -0,0 +1,81 @@
+package dev.jb0s.blockgameenhanced.manager.mmocore.vendor;
+
+import lombok.Getter;
+import lombok.Setter;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.World;
+
+public enum MMOVendor {
+    WARRIOR("Willy", "blacksmith"),
+    BLACKSMITH("Markus", "blacksmith"),
+    GUARDIAN("Armand", "blacksmith"),
+    SPELLCRAFTER("Sally", "spellhouse"),
+    ALCHEMIST("Nina", "spellhouse"),
+    SILK_WEAVER("Wendy", "spellhouse"),
+    LUMBERJACK("Larry", "woodshack"),
+    MINER("Steve", "mining"),
+    HUNTER("Hanzo", "einherjar"),
+    LEATHERWORKER("Seymour", "leathershack"),
+    BOWYER("Brent", "leathershack"),
+    REPAIR("RepairBot 3000", "repairbot"),
+    FISHERMAN("Franky", "fishing"),
+    ARTIFICER("Eitri", "runecrafter"),
+    RUNE_CARVER("Brokkr", "runecrafter"),
+    THAUMATURGE("Gregory", "healhouse"),
+    POTION_SELLER("Justin", "healhouse"),
+    WOOL_WEAVER("Porfirio", "healhouse"),
+    CHEF("Ken", "restaurant"),
+    TEA_MASTER("Piggly Wiggly", "restaurant"),
+    BOTANIST("Hesha", "botanist"),
+    ARCHAEOLOGIST("Indy", "archaeology"),
+    BARON_WARBUCKS("Baron Warbucks", "einherjar"),
+    MAYOR_MCCHEESE("Mayor McCheese", "mayor");
+
+    @Getter
+    @Setter
+    private String characterName;
+
+    @Getter
+    @Setter
+    private String ui;
+
+    MMOVendor(String name, String ui) {
+        setCharacterName(name);
+        setUi(ui);
+    }
+
+    /**
+     * Determine a MMOVendor type by their character name.
+     * @param name Name of the vendor's character.
+     * @return Vendor matching provided name, null if not found.
+     */
+    public static MMOVendor getByName(String name) {
+        for (MMOVendor x : MMOVendor.values()) {
+            if(x.getCharacterName().equals(name)) {
+                return x;
+            }
+        }
+
+         return null;
+    }
+
+    /**
+     * Find the PlayerEntity associated with this vendor type.
+     * @return PlayerEntity that is a vendor of this type
+     */
+    public PlayerEntity getVendorEntity() {
+        MinecraftClient minecraft = MinecraftClient.getInstance();
+        World world = minecraft.world; if(world == null) return null;
+
+        String convertedName = toString().toLowerCase().replace("_", " ");
+
+        for (PlayerEntity pe : world.getPlayers()) {
+            if(pe.getName().getString().toLowerCase().equals(convertedName)) {
+                return pe;
+            }
+        }
+
+        return null;
+    }
+}
--- src/main/java/dev/jb0s/blockgameenhanced/mixin/gui/screen/MixinGenericContainerScreen.java
+++ src/main/java/dev/jb0s/blockgameenhanced/mixin/gui/screen/MixinGenericContainerScreen.java
@@ -2,14 +2,20 @@ package dev.jb0s.blockgameenhanced.mixin.gui.screen;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import dev.jb0s.blockgameenhanced.BlockgameEnhanced;
+import dev.jb0s.blockgameenhanced.helper.DebugHelper;
+import dev.jb0s.blockgameenhanced.manager.mmocore.vendor.MMOVendor;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ingame.GenericContainerScreen;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.gui.screen.ingame.InventoryScreen;
 import net.minecraft.client.gui.screen.ingame.ScreenHandlerProvider;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.screen.GenericContainerScreenHandler;
 import net.minecraft.screen.slot.SlotActionType;
@@ -19,6 +25,9 @@ import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Mixin(GenericContainerScreen.class)
 public class MixinGenericContainerScreen extends HandledScreen<GenericContainerScreenHandler> implements ScreenHandlerProvider<GenericContainerScreenHandler> {
@@ -73,12 +82,45 @@ public class MixinGenericContainerScreen extends HandledScreen<GenericContainerS
      */
     @Override
     public void drawBackground(MatrixStack matrices, float delta, int mouseX, int mouseY) {
+        Identifier tex = TEXTURE;
+
+        Pattern pattern = Pattern.compile("(.*) \\(\\d/\\d\\)");
+        Matcher matcher = pattern.matcher(getTitle().getString());
+
+        MMOVendor vendor = null;
+
+        // This is a vendor, show vendor specific ui texture
+        if(matcher.matches()) {
+            String vendorName = matcher.group(1).trim();
+            vendor = MMOVendor.getByName(vendorName);
+
+            if(vendor != null) {
+                tex = new Identifier("blockgame", String.format("textures/gui/container/%s.png", vendor.getUi()));
+                backgroundWidth = 256;
+                titleX = -32;
+            }
+        }
+        else if(getTitle().getString().equals("Auction House")) {
+            tex = new Identifier("blockgame", "textures/gui/container/auction_house.png");
+            backgroundWidth = 256;
+            titleX = 8;
+        }
+
         RenderSystem.setShader(GameRenderer::getPositionTexShader);
         RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
-        RenderSystem.setShaderTexture(0, TEXTURE);
+        RenderSystem.setShaderTexture(0, tex);
         int i = (this.width - this.backgroundWidth) / 2;
         int j = (this.height - this.backgroundHeight) / 2;
         this.drawTexture(matrices, i, j, 0, 0, this.backgroundWidth, this.rows * 18 + 17);
         this.drawTexture(matrices, i, j + this.rows * 18 + 17, 0, 126, this.backgroundWidth, 96);
+
+        if(vendor != null) {
+            InventoryScreen.drawEntity(x + 192, y + 201, 26, (float)(this.x + 192) - mouseX, (float)(this.y + 203 - 50) - mouseY, client.player);
+
+            PlayerEntity vendorEntity = vendor.getVendorEntity();
+            if(vendorEntity != null) {
+                InventoryScreen.drawEntity(x - 16, y + 115, 26, (float)(this.x - 16) - mouseX, (float)(this.y + 115 - 50) - mouseY, vendorEntity);
+            }
+        }
     }
 }
