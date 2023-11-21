4279b9a224a3bd9cbb177ad2c1d0e162cf160ac1
Jake <68775263+jb0s@users.noreply.github.com>
2023-11-21 16:14:36 +0100

    Fix compiler errors.

--- src/main/java/dev/jb0s/blockgameenhanced/helper/PathHelper.java
+++ src/main/java/dev/jb0s/blockgameenhanced/helper/PathHelper.java
@@ -1,21 +1,14 @@
 package dev.jb0s.blockgameenhanced.helper;
 
-import lombok.Getter;
 import lombok.SneakyThrows;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.client.MinecraftClient;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class PathHelper {
-    private static final String MINECRAFT_FOLDER_NAME = ".minecraft";
     private static final String BLOCKGAME_FOLDER_NAME = "blockgame";
     private static final Path GAME_FOLDER_PATH = Path.of(FabricLoader.getInstance().getConfigDir() + "/"+BLOCKGAME_FOLDER_NAME);
-
-    //@Getter
-    //private static final Path minecraftFolderPath = Path.of(FabricLoader.getInstance().getConfigDir()+"../");
 
     @SneakyThrows
     public static Path getBlockgamePath() {
