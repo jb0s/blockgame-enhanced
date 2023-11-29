12eb81d29ae1fc00dd90ba04df99d5562e575c05
Jake <68775263+jb0s@users.noreply.github.com>
2023-11-29 20:24:09 +0100

    Allow for turning off gamefeatures on the cli

--- src/main/java/dev/jb0s/blockgameenhanced/BlockgameEnhancedClient.java
+++ src/main/java/dev/jb0s/blockgameenhanced/BlockgameEnhancedClient.java
@@ -26,9 +26,10 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.text.TranslatableText;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class BlockgameEnhancedClient implements ClientModInitializer {
     @Getter
@@ -60,15 +61,20 @@ public class BlockgameEnhancedClient implements ClientModInitializer {
     @Setter
     private static int latency;
 
+    @Getter
+    private List<String> disabledGameFeatures;
+
     @Override
     public void onInitializeClient() {
         BlockgameEnhanced.LOGGER.info("Welcome to Blockgame Enhanced");
         BlockgameEnhanced.LOGGER.info("Run directory: " + MinecraftClient.getInstance().runDirectory.getAbsolutePath());
+        BlockgameEnhanced.LOGGER.info("Debug Mode: " + BlockgameEnhanced.DEBUG);
 
         configManager = new ConfigManager();
         updateManager = new UpdateManager();
 
         // Load all game features
+        parseUserDisabledGameFeatures();
         loadGameFeatures();
 
         // Greet the player when they join the server
@@ -90,6 +96,19 @@ public class BlockgameEnhancedClient implements ClientModInitializer {
                 }).start();
             }
         });
+    }
+
+    /**
+     * Extracts list of disabled game features from commandline.
+     */
+    private void parseUserDisabledGameFeatures() {
+        String x = System.getProperty("nullgf");
+        if(x == null) {
+            disabledGameFeatures = new ArrayList<>();
+            return;
+        }
+
+        disabledGameFeatures = Arrays.stream(x.toLowerCase().split(",")).toList();
     }
 
     /**
@@ -115,9 +134,15 @@ public class BlockgameEnhancedClient implements ClientModInitializer {
 
         // Tick all game features after client ticks
         ClientTickEvents.END_CLIENT_TICK.register((client) -> {
+            client.getProfiler().push("tickGameFeatures");
+
             for (GameFeature gameFeature : getLoadedGameFeatures()) {
+                client.getProfiler().push(gameFeature.getClass().getSimpleName());
                 gameFeature.tick();
+                client.getProfiler().pop();
             }
+
+            client.getProfiler().pop();
         });
     }
 
@@ -126,7 +151,10 @@ public class BlockgameEnhancedClient implements ClientModInitializer {
      * @param gameFeature Instance of the game feature to load
      */
     private void loadGameFeature(GameFeature gameFeature) {
-        if(!gameFeature.isEnabled()) {
+        String name = gameFeature.getClass().getSimpleName().replace("GameFeature", "").toLowerCase();
+        boolean userDisabled = getDisabledGameFeatures().contains(name);
+
+        if(!gameFeature.isEnabled() || userDisabled) {
             BlockgameEnhanced.LOGGER.info("Skipping load of {} because it's disabled", gameFeature.getClass().getSimpleName().replace("GameFeature", " game feature"));
             return;
         }
