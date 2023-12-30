3ef83a3e6fdcadc62d1d091a6d6c4f8f604cf3c4
Jake <68775263+jb0s@users.noreply.github.com>
2023-12-30 01:28:13 +0100

    Fix pause screen

--- src/main/java/dev/jb0s/blockgameenhanced/mixin/gui/screen/MixinGameMenuScreen.java
+++ src/main/java/dev/jb0s/blockgameenhanced/mixin/gui/screen/MixinGameMenuScreen.java
@@ -1,46 +1,30 @@
 package dev.jb0s.blockgameenhanced.mixin.gui.screen;
 
 import dev.jb0s.blockgameenhanced.BlockgameEnhancedClient;
-import net.fabricmc.fabric.api.client.screen.v1.Screens;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.option.OptionsScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.gui.widget.ClickableWidget;
+import net.minecraft.client.gui.widget.GridWidget;
 import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableTextContent;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-import java.util.List;
+import java.util.function.Supplier;
 
 @Mixin(OptionsScreen.class)
-public class MixinGameMenuScreen extends Screen {
+public abstract class MixinGameMenuScreen extends Screen {
+    @Shadow protected abstract ButtonWidget createButton(Text message, Supplier<Screen> screenSupplier);
+
     protected MixinGameMenuScreen(Text title) {
         super(title);
     }
 
-    @Inject(method = "init", at = @At("RETURN"))
-    public void init(CallbackInfo ci) {
-        final List<ClickableWidget> buttons = Screens.getButtons(this);
-        for (int i = 0; i < buttons.size(); i++) {
-            ClickableWidget button = buttons.get(i);
-            boolean isResourcePackBtn = buttonTextEquals(button, "options.resourcepack");
-
-            if(isResourcePackBtn) {
-                buttons.add(i + 1, ButtonWidget.builder(Text.translatable("menu.blockgame.options"), (a) -> {
-                    assert client != null;
-
-                    Screen cfgScr = BlockgameEnhancedClient.getConfigManager().constructConfigScreen(this);
-                    client.setScreen(cfgScr);
-                }).dimensions(button.getX(), button.getY() + 24, button.getWidth(), button.getHeight()).build());
-            }
-        }
-    }
-
-    private static boolean buttonTextEquals(ClickableWidget button, String translationKey) {
-        Text text = button.getMessage();
-        return text.getContent() instanceof TranslatableTextContent && ((TranslatableTextContent) text.getContent()).getKey().equals(translationKey);
+    @Inject(method = "init", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/GridWidget;refreshPositions()V", shift = At.Shift.BY, by = -5), locals = LocalCapture.CAPTURE_FAILHARD)
+    public void init(CallbackInfo ci, GridWidget gridWidget, GridWidget.Adder adder) {
+        adder.add(createButton(Text.translatable("menu.blockgame.options"), () -> BlockgameEnhancedClient.getConfigManager().constructConfigScreen(this)));
     }
 }
